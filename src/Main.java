2465abfa9e0a5b3ee72c10c9276e02c1d5eda9cc
Artur Iablokov <artyapple@gmail.com>
2017-04-23 00:21:18 +0200

    Quicksort tests

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,42 @@
+import element.Element;
+import pivot.IPivotElement;
+import pivot.PivotRandom;
+import sort.QuickSort;
+import sort.SortingAlgorithm;
 
 public class Main {
 
+	
+    public static void main(String[] args){
+    
+    	IPivotElement p = new PivotRandom();
+		SortingAlgorithm alg = new QuickSort(p);
+		Element<String>[] array = initData();
+		
+		printArray(array);
+		alg.sort(array);
+		printArray(array);
+		
+    }
+    
+    private static Element<String>[] initData(){
+    	Element<String>[] array = new Element[9];
+    	int keys[] = new int[]{4,0,1,-1,0,2,-5,0,4};
+    	int i = 0;
+    	for(int key : keys){
+    		array[i]=new Element<String>(key, "");
+    		i++;
+    	}
+    	return array;
+    }
+    
+    public static <T> void printArray(Element<T>[] array){
+    	System.out.print("Array is:");
+    	for(int i = 0; i < array.length; i++){
+    		System.out.print(array[i].getKey()+", ");
+    	}
+    	System.out.println("\n");
+    	
+    }
 }
+
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,38 +1,159 @@
 package sort;
+
+import java.awt.image.PixelInterleavedSampleModel;
+
+import element.Element;
 import element.IElement;
+import pivot.IPivotElement;
+import sun.applet.Main;
 
 public class QuickSort implements SortingAlgorithm {
 
-	@Override
-	public <T> void sort(IElement<T>[] a) {
-		// TODO Auto-generated method stub
+	private IPivotElement p;
 
+	public QuickSort(IPivotElement p) {
+		this.p = p;
 	}
 
-	private <T> void quicksort(int ilinks, int irechts, IElement<T>[] a) {
-		long pivot;
-		int i, j;
-		IElement<T> tmp;
-		if (irechts > ilinks) {
-			i = ilinks;
-			j = irechts - 1;
-			pivot = a[irechts].getKey();
-			while (true) {
-				while (a[i].getKey() < pivot) {
-					i++;
-				}
-				while (a[j].getKey() >= pivot) {
-					j--; // Vorsicht:
-							// Stop-Element einbauen
-				}
-				if (i >= j)
-					break; // in der Mitte getroffen
-				swap(i, j, a); // vertauschen
-			}
-			swap(i, irechts, a);// Pivotelement in die Mitte tauschen
-			quicksort(ilinks, i - 1, a);
-			quicksort(i + 1, irechts, a);
+	@Override
+	public <T> void sort(IElement<T>[] a) {
+		if (a == null || a.length == 0) {
+			return;
 		}
+		quicksort(0, a.length - 1, a);
+	}
+
+	// private <T> void quicksort(int ilinks, int irechts, IElement<T>[] a) {
+	//
+	// if (irechts <= ilinks) {
+	// return;
+	// }
+	//
+	// long pivot;
+	// int i, j, pivIndex;
+	//
+	// i = ilinks;
+	// j = irechts;
+	// pivIndex = p.getPivotIndex(ilinks, irechts);
+	// pivot = a[pivIndex].getKey();
+	//
+	// while (true) {
+	// //while ((i < pivIndex) && (a[i].getKey() <= pivot)) {
+	// while (a[i].getKey() < pivot) {
+	// i++;
+	// }
+	// //while ((j > pivIndex) && (a[j].getKey() >= pivot)) {
+	// while (a[j].getKey() >= pivot) {
+	// if(j>0){
+	// j--; // Vorsicht: Stop-Element einbauen
+	// } else {
+	// break;
+	// }
+	// }
+	// if (i >= j) break;
+	// swap(i, j, a); // vertauschen
+	//
+	// // if (i == pivIndex) // wenn true, dann wurde das Pivot nach rechts
+	// // verschoben
+	// // pivIndex = j;
+	// // else if (j == pivIndex) // wenn true, dann wurde das Pivot nach
+	// // links verschoben
+	// // pivIndex = i;
+	// // }
+	// }
+	// //swap(i, pivIndex, a);// Pivotelement in die Mitte tauschen
+	// quicksort(ilinks, i-1, a);
+	// quicksort(i+1, pivIndex, a);
+	// }
+
+	private <T> void quicksort(int ilinks, int irechts, IElement<T>[] a) {
+		int i = ilinks, j = irechts;
+		// Get the pivot element from the middle of the list
+		// int pivot = a[low + (high-low)/2];
+		int pivIndex = p.getPivotIndex(ilinks, irechts);
+		long pivot = a[pivIndex].getKey();
+
+		// Divide into two lists
+		while (i <= j) {
+			// If the current value from the left list is smaller than the pivot
+			// element then get the next element from the left list
+			while (a[i].getKey() < pivot) {
+				i++;
+			}
+			// If the current value from the right list is larger than the pivot
+			// element then get the next element from the right list
+			while (a[j].getKey() > pivot) {
+				j--;
+			}
+
+			// If we have found a value in the left list which is larger than
+			// the pivot element and if we have found a value in the right list
+			// which is smaller than the pivot element then we exchange the
+			// values.
+			// As we are done we can increase i and j
+			if (i <= j) {
+				swap(i, j, a);
+				i++;
+				j--;
+			}
+		}
+		// Recursion
+		if (ilinks < j)
+			quicksort(ilinks, j, a);
+		if (i < irechts)
+			quicksort(i, irechts, a);
+	}
+
+	// }
+	// if (ilinks < irechts) {
+	// int p = part(ilinks, irechts, a);
+	// quicksort(ilinks, p - 1, a);
+	// quicksort(p, irechts, a);
+	// }
+	// }
+
+	private <T> int part(int ilinks, int irechts, IElement<T>[] a) {
+		long pivot;
+		int i, j, pivIndex;
+
+		i = ilinks;
+		j = irechts;
+		pivIndex = p.getPivotIndex(ilinks, irechts);
+		pivot = a[pivIndex].getKey();
+
+		System.out.println("pivot index: " + pivIndex + " pivElement " + pivot);
+		boolean pr = true;
+
+		while (i < j) {
+			while (a[i].getKey() < pivot) {
+				i++;
+			}
+			while (a[j].getKey() > pivot) {
+				j--;
+			}
+			if (i <= j) {
+				swap(i, j, a);
+				if ((a[i].getKey() == a[j].getKey()) && (a[i].getKey() == pivot)) {
+					if (pr) {
+						printArray(a);
+						System.out.println("i and j and pivot are equals");
+						System.out.println("i=" + i + "and j=" + j);
+						pr = false;
+					}
+
+					if (i + 1 == j) {
+						break;
+					} else if (i == j) {
+						break;
+					} else {
+						swap(i + 1, j, a);
+					}
+
+				}
+			}
+		}
+
+		return i + 1;
 	}
 
 	private <T> void swap(int i, int j, IElement<T>[] a) {
@@ -42,4 +163,13 @@ public class QuickSort implements SortingAlgorithm {
 		a[j] = tmp;
 	}
 
+	// private
+
+	private <T> void printArray(IElement<T>[] a) {
+		System.out.print("Array is:");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i].getKey() + ", ");
+		}
+		System.out.println("\n");
+	}
 }
