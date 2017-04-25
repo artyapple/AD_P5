7813158b7fde5f21c1e44d6f6f5cc4121486e693
Artur Iablokov <artyapple@gmail.com>
2017-04-25 17:29:05 +0200

    Added: Complexity Analysis

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,11 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import element.Element;
 import pivot.IPivotElement;
+import pivot.PivotFirst;
 import pivot.PivotMedian;
 import pivot.PivotRandom;
 import sort.QuickSort;
@@ -7,45 +13,10 @@ import sort.SortingAlgorithm;
 
 public class Main {
 
-	
-    public static void main(String[] args){
-    
-    	IPivotElement p = new PivotRandom();
-		SortingAlgorithm alg = new QuickSort(p);
-		Element<String>[] array = initData();
-		
-//		printArray(array);
-//		alg.sort(array);
-//		printArray(array);
-		
-		IPivotElement p2 = new PivotMedian();
-		alg = new QuickSort(p2);
-		array = initData();
-		
-		printArray(array);
-		alg.sort(array);
-		printArray(array);
-		
-    }
-    
-    private static Element<String>[] initData(){
-    	Element<String>[] array = new Element[9];
-    	int keys[] = new int[]{4,0,1,-1,0,2,-5,0,4};
-    	int i = 0;
-    	for(int key : keys){
-    		array[i]=new Element<String>(key, "");
-    		i++;
-    	}
-    	return array;
-    }
-    
-    public static <T> void printArray(Element<T>[] array){
-    	System.out.print("Array is:");
-    	for(int i = 0; i < array.length; i++){
-    		System.out.print(array[i].getKey()+", ");
-    	}
-    	System.out.println("\n");
-    	
-    }
+	public static void main(String[] args) {
+		ComplexityAnalysis a = new ComplexityAnalysis();
+		a.avgCase();
+		//a.bestCaseMedianPivot();
+		//a.worstCaseFirstPivot();
+	}
 }
-
